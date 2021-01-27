fd7338577b7840f3a4107f536bec7912e689d58e
unknown <burakyilmz17@gmail.com>
2021-01-28 00:08:26 +0300

    functions added

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345@localhost:5432/api"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://burak:12345@localhost:5432/api"
 
 # Inıt db
 db = SQLAlchemy(app)
@@ -45,32 +45,44 @@ def add_product():
     description = request.json['description']
     price = request.json['price']
     qty = request.json['qty']
-
     new_product = Product(name, description, price, qty)
-
     db.session.add(new_product)
     db.session.commit()
-
     return product_schema.jsonify(new_product)
 
+@app.route('/product', methods=['GET'])
+def get_products():
+  all_products = Product.query.all()
+  result = product_schemas.dump(all_products)
+  return jsonify(result)
 
-    # perm_address = request.form['Permanent address']
-    # curr_address = request.form['current Address']
-    # mobile = request.form['Mobile']
-    # name = request.form['Name']
-    # pets = request.form['Pets']
 
-    # new_usr = {
-    #     'Address':{
-    #         'Permanent address':perm_address,
-    #         'current Address':curr_address
-    #     },
-    #     'Boolean':true,
-    #     'Mobile':mobile,
-    #     'Name':name,
-    #     'Pets':pets
-    # }
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+  product = Product.query.get(id)
+  return product_schema.jsonify(product)
 
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+    product.name = name
+    product.description = description
+    product.price = price
+    product.qty = qty
+    new_product = Product(name,description,price,qty)
+    db.session.commit()
+    return product_schema.jsonify(new_product)
+
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+  product = Product.query.get(id)
+  db.session.delete(product)
+  db.session.commit()
+  return product_schema.jsonify(product)
     
 
 if __name__ == '__main__':
